785cf49d66d9bc73557cb0988d91b0219911e556
themandysingh <mandy98@outlook.com>
2019-12-31 18:34:17 +0530

    Scratch: Added instance variables for information being scraped except match events. Changed team lineup storage format from a list to a dictionary with shirt number as key and Player object as value.

--- scraping/match_centre_scraping_utility.py
+++ scraping/match_centre_scraping_utility.py
@@ -13,6 +13,25 @@ class Match_Center_Scraping:
     LINEUPS_BUTTON_CONFIRMATION = '#mainContent > div > section > div.centralContent > div.mcTabsContainer > div.mcTabs > section.squads.mcMainTab.active > div > div > div.teamList.mcLineUpContainter.homeLineup.active > div > div > ul:nth-child(2) > li > a > div.info > span.nationality > span.playerCountry'
 
     def __init__(self, MATCH_URL):
+        # Team Names
+        self.home_team_name = None
+        self.away_team_name = None
+
+        # Match Info
+        self.date = None
+        self.referee = None
+        self.attendance = None
+        self.stadium = None
+        self.score = None
+        
+        # Team Lineups
+        self.home_captain = None
+        self.away_captain = None
+        self.home_starting_eleven = None
+        self.away_starting_eleven = None
+        self.home_subs = None
+        self.away_subs = None
+
         self.match_url = MATCH_URL
         self.driver = webdriver.Chrome('/Users/mandy/soccer-analytics/scraping/chromedriver')
         self.driver.implicitly_wait(30)
@@ -64,43 +83,44 @@ class Match_Center_Scraping:
         lineup_containers = self.lineups_page_soup.findAll('div', {'class':'matchLineupTeamContainer'})
         home_team_container = lineup_containers[0]
         away_team_container = lineup_containers[1]
-        home_starting_eleven, home_subs = self.extract_players(home_team_container)
-        away_starting_eleven, away_subs = self.extract_players(away_team_container)
+        home_starting_eleven, home_subs = self.extract_players(home_team_container, 'home')
+        away_starting_eleven, away_subs = self.extract_players(away_team_container, 'away')
         return [home_starting_eleven, home_subs], [away_starting_eleven, away_subs]
 
-    def extract_players(self, team_container):
+    def extract_players(self, team_container, side):
         team_container_children = team_container.findChildren(recursive=False)
         position = None
-        starting_eleven = [None] * 11
-        starting_eleven_count = 0
-        substitutes = [None] * 7
-        substitutes_count = 0
+        starting_eleven = {}
+        substitutes = {}
         for child in team_container_children:
             if child.name == 'h3':
                 position = child.text
             else:
                 player_lines = child.findAll('li')
                 for line in player_lines:
-                    player = self.create_player_profile(line, position)
+                    player = self.create_player_profile(line, position, side)
                     
                     if position == 'Substitutes':
-                        substitutes[substitutes_count] = player
-                        substitutes_count += 1
+                        substitutes[player.number] = player
                     else:
-                        starting_eleven[starting_eleven_count] = player
-                        starting_eleven_count += 1
+                        starting_eleven[player.number] = player
+
                     
         return starting_eleven, substitutes
 
-    def create_player_profile(self, line, position):
+    def create_player_profile(self, line, position, side):
         player = Player()
         player_info_divs = line.a.findAll('div')
-        player.number = player_info_divs[0].text.split(' ')[2]
+        player.number = int(player_info_divs[0].text.split(' ')[2])
         player.name = player_info_divs[1].find('span', {'class':'name'}).text.replace(player_info_divs[1].find('span', {'class':'name'}).div.text, '')
         player.nationality = player_info_divs[1].find('span', {'class':'playerCountry'}).text
         player.position = position
-        if len(player_info_divs) > 2:
+        if len(player_info_divs) > 3:
             player.captain = True
+            if side == 'home':
+                self.home_captain = player
+            else:
+                self.away_captain = player
         
         if position == 'Substitutes':
             player.position = player_info_divs[1].find('span', {'class':'position'}).text
@@ -110,7 +130,8 @@ class Match_Center_Scraping:
     def get_teams(self):
         stats_table = self.stats_page_soup.find('tbody', {'class': 'matchCentreStatsContainer'}).parent
         teams = stats_table.thead.findAll("a")
-        return teams[0].text.strip(), teams[1].text.strip()
+        self.home_team_name = teams[0].text.strip()
+        self.away_team_name = teams[1].text.strip()
 
     def get_team_stats(self):
         stats_table = self.stats_page_soup.find('tbody', {'class': 'matchCentreStatsContainer'}).parent
@@ -125,17 +146,16 @@ class Match_Center_Scraping:
 
     def get_match_info(self):
         infoBar = self.stats_page_soup.find('div', {'class': 'matchInfo'})
-        date = infoBar.find('div', {'class':'matchDate'}).text
-        referee = infoBar.find('div', {'class':'referee'}).text.strip()
-        stadium = infoBar.find('div', {'class':'stadium'}).text
-        attendance = infoBar.find('div', {'class':'attendance'}).text.split(' ')[1]
-        score = self.stats_page_soup.find('div', {'class': 'score'}).text.split('-')
-
-        return date, referee, stadium, attendance, score
+        self.date = infoBar.find('div', {'class':'matchDate'}).text
+        self.referee = infoBar.find('div', {'class':'referee'}).text.strip()
+        self.stadium = infoBar.find('div', {'class':'stadium'}).text
+        self.attendance = infoBar.find('div', {'class':'attendance'}).text.split(' ')[1]
+        self.score = self.stats_page_soup.find('div', {'class': 'score'}).text.split('-')
 
 
 if __name__ == '__main__':
 	match_center = Match_Center_Scraping('https://www.premierleague.com/match/46610')
+
 home_events, away_events = match_center.get_match_events()
 for home_event in home_events:
     print(home_event.name, home_event.time)
@@ -143,25 +163,4 @@ print()
 print('away stuff')
 for away_event in away_events:
     print(away_event.name, away_event.time)
-# home_lineup, away_lineup = match_center.get_lineups()
-# for player in home_lineup[0]:
-#     print(player.name)
-
-# print()
-# print('Substitutes:')
-# for player in home_lineup[1]:
-#     print(player.name)
-
-# print()
-
-# for player in away_lineup[0]:
-#     print(player.name)
-
-# print()
-# print('Substitutes:')
-# for player in away_lineup[1]:
-#     print(player.name)
-    # print(match_center.get_teams())
-	# print(match_center.get_team_stats())
-	# print(match_center.get_match_info())
 
